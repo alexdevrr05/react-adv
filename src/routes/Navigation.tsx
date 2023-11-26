ce156df8dab8701218aee54ad2fc0e882b2fd057
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2023-11-26 12:41:14 -0600

    Add Suspense component to Navigation

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import {
   BrowserRouter,
   Routes,
@@ -12,36 +13,38 @@ import logo from '../logo.svg';
 
 export const Navigation = () => {
   return (
-    <BrowserRouter>
-      <div className='main-layout'>
-        <nav>
-          <img src={logo} alt='React Logo' />
-          <ul>
-            {routes.map(({ path, to, name }) => (
-              <li key={path}>
-                <NavLink
-                  to={to}
-                  className={({ isActive }) => (isActive ? 'nav-active' : '')}
-                >
-                  {name}
-                </NavLink>
-              </li>
+    <Suspense fallback={<span>Loading...</span>}>
+      <BrowserRouter>
+        <div className='main-layout'>
+          <nav>
+            <img src={logo} alt='React Logo' />
+            <ul>
+              {routes.map(({ path, to, name }) => (
+                <li key={path}>
+                  <NavLink
+                    to={to}
+                    className={({ isActive }) => (isActive ? 'nav-active' : '')}
+                  >
+                    {name}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
+
+          <Routes>
+            {routes.map((route) => (
+              <Route
+                key={route.to}
+                path={route.path}
+                element={<route.Component />} // <></>
+              />
             ))}
-          </ul>
-        </nav>
 
-        <Routes>
-          {routes.map((route) => (
-            <Route
-              key={route.to}
-              path={route.path}
-              element={<route.Component />} // <></>
-            />
-          ))}
-
-          <Route path='/*' element={<Navigate to={routes[0].to} replace />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+            <Route path='/*' element={<Navigate to={routes[0].to} replace />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </Suspense>
   );
 };
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,17 +1,25 @@
-import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
+import { lazy } from 'react';
+import { LazyPage2, LazyPage3 } from '../01-lazyload/pages';
+
+type JSXComponent = () => JSX.Element;
 
 interface Route {
   to: string;
   path: string;
-  Component: () => JSX.Element;
+  // Component: () => JSX.Element;
+  Component: React.LazyExoticComponent<JSXComponent> | JSXComponent;
   name: string;
 }
+
+const Lazy1 = lazy(() => import('../01-lazyload/pages/LazyPage1'));
+// const Lazy2 = lazy(() => import('../01-lazyload/pages/LazyPage2'));
+// const Lazy3 = lazy(() => import('../01-lazyload/pages/LazyPage3'));
 
 export const routes: Route[] = [
   {
     to: '/lazy1',
     path: 'lazy1',
-    Component: LazyPage1,
+    Component: Lazy1,
     name: 'Lazy-1',
   },
   {
