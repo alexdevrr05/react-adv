cb8b960b3e33feaf04d264cca97ebc11da0feca0
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2023-11-26 12:27:04 -0600

    Update default route in Navigation component

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -39,7 +39,7 @@ export const Navigation = () => {
             />
           ))}
 
-          <Route path='/*' element={<Navigate to='/home' replace />} />
+          <Route path='/*' element={<Navigate to={routes[0].to} replace />} />
         </Routes>
       </div>
     </BrowserRouter>
